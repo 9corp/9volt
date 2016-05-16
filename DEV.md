6b58fcad0c53f01c4fcdf07c140f7b0263b75115
Daniel Selans <daniel.selans@gmail.com>
2016-05-15 23:30:05 -0700

    more brainstorming; updating docs with random thoughts

--- DEV.md
+++ DEV.md
@@ -7,8 +7,21 @@
 - You should be good to go!
 
 ### Random musings
-- Make an effort to use interfaces - it will make testing *a lot* easier
-- Speaking of testing - use `counterfeiter` to generate fakes
-    + https://github.com/maxbrunsfeld/counterfeiter
 - Use `gofmt`
-- Make an effort to ensure everything is unit testable
+- Use the built-in race detector (`go run -race`)
+- `foo.New()` should never error; treat as constructor
+- `foo.Start()` for things that will complete (ie. may launch goroutines, etc.)
+- `foo.Run()` for things that will *not* complete and should (probably) be launched in a goroutine
+- Use bailout blocks/negative logic/negated if blocks when possible
+- Make an effort to use interfaces - it will make testing *a lot* easier
+- Using interfaces will allow you to use `counterfeiter` for generating fakes
+    + https://github.com/maxbrunsfeld/counterfeiter
+- At minimum, use an assertion library when writing tests (ie. https://github.com/stretchr/testify)
+- Make an effort to ensure everything is unit testable first
+- Tests should be tagged as follows (at the top of each test file):
+    + For unit tests
+        * `// +build unit`
+    + For integration tests
+        * `// +build integration`
+    + For functional tests
+        * `// +build functional`
--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 var (
 	listenAddress = kingpin.Flag("listen", "Address for 9volt's API to listen on").Short('l').Default("0.0.0.0:8080").String()
 	etcdPrefix    = kingpin.Flag("etcd-prefix", "Prefix that 9volt's configuration is stored under in etcd").Short('p').Default("9volt").String()
-	etcdHosts     = kingpin.Flag("etcd-members", "List of etcd cluster members").Short('e').Strings()
+	etcdMembers   = kingpin.Flag("etcd-members", "List of etcd cluster members").Short('e').Strings()
 	debug         = kingpin.Flag("debug", "Enable debug mode").Short('d').Bool()
 
 	version string
@@ -34,16 +34,23 @@ func main() {
 	var wg sync.WaitGroup
 	wg.Add(1)
 
+	cfg := config.New(*listenAddress, *etcdPrefix, *etcdMembers)
+
+	if err := cfg.Load(); err != nil {
+		log.Fatalf("Unable to load configuration from etcd: %v", err.Error())
+	}
+
 	// Naming convention; intended module purpose
 
-	// api      --  main API entry point
-	// director --  performs check distribution
-	// manager  --  manages check lifetime
-	// cluster  --  performs leader election; heartbeat
-	// monitor  --  perform actual monitoring
-	// fetcher  --  fetch statistics from outside sources
-	// alerter  --  send alerts to various destinations
-	// state    --  periodically dump state to etcd
+	// api       --  main API entry point
+	// director  --  performs check distribution
+	// manager   --  manages check lifetime
+	// cluster   --  performs leader election; heartbeat
+	// monitor   --  perform actual monitoring
+	// fetcher   --  fetch statistics from outside sources
+	// alerter   --  send alerts to various destinations
+	// state     --  periodically dump state to etcd
+	// config    --  configuration validation and loading
 
 	wg.Wait()
 }
