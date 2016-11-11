621bfe205512bffbf4dfe61e53eb372a8370e457
Daniel Selans <daniel.selans@gmail.com>
2016-11-10 23:35:44 -0800

    code dedup for monitor; not sure if there is a need for every checker to have their own struct - might be more room for additional simplification

--- monitor/http_monitor.go
+++ monitor/http_monitor.go
@@ -5,40 +5,23 @@ import (
 )
 
 type HTTPMonitor struct {
-	RMC        *RootMonitorConfig
-	identifier string
+	Base
 }
 
 func NewHTTPMonitor(rmc *RootMonitorConfig) IMonitor {
-	return &HTTPMonitor{
-		RMC:        rmc,
-		identifier: "http",
-	}
-}
-
-func (h *HTTPMonitor) Run() error {
-	log.Debugf("%v-%v: Starting work for monitor %v...", h.Identifier(), h.RMC.GID, h.RMC.Name)
-
-	for t := range h.RMC.Ticker.C {
-		// execute the check
-		log.Warningf("%v-%v: Tick at %v", h.Identifier(), h.RMC.GID, t.String())
-
-		h.httpCheck()
+	h := &HTTPMonitor{
+		Base: Base{
+			RMC:        rmc,
+			identifier: "http",
+		},
 	}
 
-	log.Debugf("%v-%v: Goroutine has been stopped for monitor %v; exiting...", h.Identifier(), h.RMC.GID, h.RMC.Name)
-	return nil
+	h.monitorFunc = h.httpCheck
+
+	return h
 }
 
 func (h *HTTPMonitor) httpCheck() *Response {
 	log.Debugf("%v-%v: Performing http check!", h.Identifier(), h.RMC.GID)
 	return nil
-}
-
-func (h *HTTPMonitor) Identifier() string {
-	return h.identifier
-}
-
-func (h *HTTPMonitor) Stop() {
-	h.RMC.Ticker.Stop()
 }
