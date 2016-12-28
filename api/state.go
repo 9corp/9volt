d5a721454dbbad6d5a50b376ce03b2eef3747789
Daniel Selans <daniel.selans@gmail.com>
2016-12-27 19:38:38 -0800

    quick poc for state api endpoint

--- api/state.go
+++ api/state.go
@@ -1,14 +1,38 @@
 package api
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/InVisionApp/rye"
+
+	"github.com/9corp/9volt/dal"
 )
 
 func (a *Api) StateHandler(rw http.ResponseWriter, r *http.Request) *rye.Response {
-	fmt.Fprintf(rw, "state handler")
+	stateData, err := a.Config.DalClient.Get("state", &dal.GetOptions{
+		Recurse: true,
+	})
+
+	if err != nil {
+		rye.WriteJSONStatus(rw, "error", fmt.Sprintf("Unable to fetch state: %v", err), http.StatusInternalServerError)
+		return nil
+	}
+
+	fullResponse := make([]json.RawMessage, 0)
+
+	for _, v := range stateData {
+		fullResponse = append(fullResponse, []byte(v))
+	}
+
+	data, err := json.Marshal(fullResponse)
+	if err != nil {
+		rye.WriteJSONStatus(rw, "error", "crap", http.StatusInternalServerError)
+		return nil
+	}
+
+	rye.WriteJSONResponse(rw, http.StatusOK, data)
 	return nil
 }
 
