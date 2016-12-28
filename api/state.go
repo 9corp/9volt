90529b00605fad068b3b9c1ed0544f649501d62b
Daniel Selans <daniel.selans@gmail.com>
2016-12-28 00:00:40 -0800

    regular state fetch + state w/ tags is complete

--- api/state.go
+++ api/state.go
@@ -1,40 +1,24 @@
 package api
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
 	"strings"
 
 	"github.com/InVisionApp/rye"
 	// log "github.com/Sirupsen/logrus"
-
-	"github.com/9corp/9volt/dal"
 )
 
 func (a *Api) StateHandler(rw http.ResponseWriter, r *http.Request) *rye.Response {
-	stateData, err := a.Config.DalClient.Get("state", &dal.GetOptions{
-		Recurse: true,
-	})
-
+	stateData, err := a.Config.DalClient.FetchState()
 	if err != nil {
-		rye.WriteJSONStatus(rw, "error", fmt.Sprintf("Unable to fetch state: %v", err), http.StatusInternalServerError)
-		return nil
+		return &rye.Response{
+			Err:        fmt.Errorf("Unable to fetch state data: %v", err),
+			StatusCode: http.StatusInternalServerError,
+		}
 	}
 
-	fullResponse := make([]json.RawMessage, 0)
-
-	for _, v := range stateData {
-		fullResponse = append(fullResponse, []byte(v))
-	}
-
-	data, err := json.Marshal(fullResponse)
-	if err != nil {
-		rye.WriteJSONStatus(rw, "error", "crap", http.StatusInternalServerError)
-		return nil
-	}
-
-	rye.WriteJSONResponse(rw, http.StatusOK, data)
+	rye.WriteJSONResponse(rw, http.StatusOK, stateData)
 	return nil
 }
 
@@ -47,8 +31,14 @@ func (a *Api) StateWithTagsHandler(rw http.ResponseWriter, r *http.Request) *rye
 
 	tags := strings.Split(vals["tags"][0], ",")
 
-	fmt.Fprintf(rw, "Our tags: %v", tags)
-	fmt.Fprintf(rw, "Tag length: %v", len(tags))
+	stateData, err := a.Config.DalClient.FetchStateWithTags(tags)
+	if err != nil {
+		return &rye.Response{
+			Err:        fmt.Errorf("Unable to fetch state data: %v", err),
+			StatusCode: http.StatusInternalServerError,
+		}
+	}
 
+	rye.WriteJSONResponse(rw, http.StatusOK, stateData)
 	return nil
 }
--- dal/dal.go
+++ dal/dal.go
@@ -4,11 +4,16 @@ package dal
 import (
 	"context"
 	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"path"
+	"reflect"
 	"time"
 
+	log "github.com/Sirupsen/logrus"
 	"github.com/coreos/etcd/client"
+
+	"github.com/9corp/9volt/util"
 )
 
 type IDal interface {
@@ -29,6 +34,8 @@ type IDal interface {
 	FetchAllMemberRefs() (map[string]string, error)
 	FetchCheckStats() (map[string]int, error)
 	FetchAlerterConfig(string) (string, error)
+	FetchState() ([]byte, error)
+	FetchStateWithTags([]string) ([]byte, error)
 }
 
 type GetOptions struct {
@@ -44,6 +51,13 @@ type Dal struct {
 	KeysAPI client.KeysAPI
 	Members []string
 	Prefix  string
+}
+
+// Helper struct for findings tags in a state config blob
+type simpleState struct {
+	Config struct {
+		Tags []string `json:"tags"`
+	} `json:"config"`
 }
 
 func New(prefix string, members []string) (*Dal, error) {
@@ -355,4 +369,104 @@ func (d *Dal) FetchAlerterConfig(alertKey string) (string, error) {
 // wrapper for etcd client's KeyNotFound error
 func (d *Dal) IsKeyNotFound(err error) bool {
 	return client.IsKeyNotFound(err)
+}
+
+// Wrapper for fetching state information
+func (d *Dal) FetchState() ([]byte, error) {
+	stateData, err := d.Get("state", &GetOptions{
+		Recurse: true,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	combinedData := make([]json.RawMessage, 0)
+
+	for _, v := range stateData {
+		combinedData = append(combinedData, []byte(v))
+	}
+
+	full, err := json.Marshal(combinedData)
+	if err != nil {
+		return nil, fmt.Errorf("Unable to convert JSON state data: %v", err)
+	}
+
+	return full, nil
+}
+
+// Wrapper for fetching state information that is tagged with `tags`
+func (d *Dal) FetchStateWithTags(tags []string) ([]byte, error) {
+	stateData, err := d.FetchState()
+	if err != nil {
+		return nil, err
+	}
+
+	filteredDataset := make([]json.RawMessage, 0)
+	var dataset []map[string]interface{}
+
+	if err := json.Unmarshal(stateData, &dataset); err != nil {
+		return nil, fmt.Errorf("Unable to perform initial state data conversion: %v", err)
+	}
+
+	for _, entry := range dataset {
+		found, err := d.stateContainsConfigWithTags(entry, tags)
+		if err != nil {
+			log.Errorf("Unable to determine if state entry '%v' contains our wanted tags: %v", entry, err)
+			continue
+		} else if !found {
+			continue
+		}
+
+		// This entry contains the tags we are looking for; remarshal and populate
+		// our filtered data set
+		remarshalled, err := json.Marshal(entry)
+		if err != nil {
+			return nil, fmt.Errorf("Unable to re-marshal filtered dataset: %v", err)
+		}
+
+		filteredDataset = append(filteredDataset, remarshalled)
+	}
+
+	// final remarshal
+	final, err := json.Marshal(filteredDataset)
+	if err != nil {
+		return nil, fmt.Errorf("Unable to perform final filtered dataset marshal: %v", err)
+	}
+
+	return final, nil
+}
+
+// Helper for determining if a state entry has a "config" with "tags" slice
+func (d *Dal) stateContainsConfigWithTags(entry map[string]interface{}, tags []string) (bool, error) {
+	if _, ok := entry["config"]; !ok {
+		return false, nil
+	}
+
+	configs, found := entry["config"].(map[string]interface{})
+	if !found {
+		return false, nil
+	}
+
+	if _, ok := configs["tags"]; !ok {
+		return false, nil
+	}
+
+	s := reflect.ValueOf(configs["tags"])
+	if s.Kind() != reflect.Slice {
+		return false, nil
+	}
+
+	// populate tags we've discovered
+	foundTags := make([]string, s.Len())
+
+	for i := 0; i < s.Len(); i++ {
+		foundTags[i] = s.Index(i).Elem().String()
+	}
+
+	if util.StringSliceInStringSlice(tags, foundTags) {
+		return true, nil
+	}
+
+	return false, nil
 }
