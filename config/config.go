3820c1ee991ff6cabeef6c137a283db748e810db
Daniel Selans <daniel.selans@gmail.com>
2017-02-06 21:22:23 -0800

    upadte member status to contain tags

--- config/config.go
+++ config/config.go
@@ -31,6 +31,10 @@ type serverConfig struct {
 
 // Pass in the dal client in order to facilitate better/easier testing story
 func New(memberID, listenAddress, etcdPrefix string, etcdMembers, tags []string, dalClient dal.IDal, eqClient *event.Client) *Config {
+	if tags == nil {
+		tags = make([]string, 0)
+	}
+
 	cfg := &Config{
 		ListenAddress: listenAddress,
 		EtcdPrefix:    etcdPrefix,
